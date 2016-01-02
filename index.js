774ab89008e638f0961b7fcf07606616dc67dad0
Anodia <Anodia@raspberry.pi>
2016-01-02 15:28:42 +0000

    Plein de trucs wesh

--- index.js
+++ index.js
@@ -2,15 +2,31 @@
 var Anodia;
 var five = require("johnny-five");
 var SerialPort = require("serialport").SerialPort;
+var fs = require("fs");
 
 var Communication_Arduino = {
 
 	arduino: null,
+	map : null,
 
 	init: function(anodia,callback) {
 
 		Anodia = anodia;
 		this.arduino = new five.Board({repl:false,debug:false});
+
+		this.arduino.sensors = [];
+		this.arduino.relays = [];
+
+		this.arduino.get = function(pin) {
+
+			if (this.sensors[pin]) {
+				return this.sensors[pin];
+			} else if (this.relays[pin]) {
+				return this.relays[pin];
+			}
+
+			return null;
+		};
 
 		this.arduino.on("error", function(msg) {
 			Anodia.log(1,msg);
@@ -22,10 +38,9 @@ var Communication_Arduino = {
 
 		this.arduino.on("ready", function() {
 
-			this.arduino.sensor = new five.Sensor({ pin: "A0", freq: 1000});
-
 			Anodia.log(2,"Arduino is ready");
-			callback(null);
+
+			this.load_map(callback);
 		}.bind(this));
 	},
 
@@ -33,13 +48,81 @@ var Communication_Arduino = {
 
 	},
 
+	load_map: function(callback) {
+		Anodia.log(2,"loading_map");
+
+		this.map = JSON.parse(fs.readFileSync("./config/map.json"));
+
+		for(var key in this.map.sensors) {
+			sensor = this.map.sensors[key];
+			this.arduino.sensors[sensor.pin] = this.activate_sensor(sensor);
+		}
+
+		for(var key in this.map.relays) {
+			relay = this.map.relays[key];
+			this.arduino.relays[relay.pin] = this.activate_relay(relay);
+		}
+
+		callback(null);
+	},
+
+	activate_sensor: function(sensor) {
+
+		var sensor = new five.Sensor({ pin: sensor.pin, freq: sensor.freq, type: sensor.type || "analog"});
+
+		sensor._value = function() {
+			return this.raw;
+		};
+
+		sensor._set = function() {
+			return "You cannot set a value to a sensor";
+		};
+
+		Anodia.log(2,"new sensor pin: "+sensor.pin);
+
+		return sensor;
+	},
+
+	activate_relay: function(relay) {
+		var relay = new five.Relay({ pin: relay.pin, type: relay.type });
+
+		relay._value = function() {
+			return this.isOn; // Why '!' ? I dunno, but it works 
+		};
+
+		relay._set = function(value) {
+			if (value == "true" || value == "1") {
+				this.close();
+				return true;
+			} else if (value == "false" || value == "0") {
+				this.open();
+				return false;
+			} else {
+				return "Value must be 'true' or 'false'";
+			}
+		};
+
+		relay._reset = function() {
+			this._set(this.type == "NC" ? "true" : "false");
+		};
+
+		Anodia.log(2,"new relay pin: "+relay.pin);
+
+		return relay;
+	},
+
 	command: function(args) {
 		var pin = args.shift();
 		var action = args.shift();
 
-		if (pin == "A0") {
-			if (action == "value") {
-				console.log(this.temperature(this.arduino.sensor.raw));
+		if (this.arduino.get(pin)) {
+			pin = this.arduino.get(pin);
+
+			if (pin["_"+action]) {
+			//if (action == "value") {
+				var ret = pin["_"+action](args);
+				if (ret != undefined)
+					console.log(ret);
 			} else {
 				console.log("Unknown action: "+action);
 			}
@@ -47,6 +130,16 @@ var Communication_Arduino = {
 			console.log("Unknown pin: "+pin);
 		}
 	},
+	exit: function(cb) {
+		for(var key in this.arduino.relays) {
+			this.arduino.relays[key]._set("false");
+		}
+
+		setTimeout(function() {
+			// To be sure everything on serial port has been transmitted
+			cb(null);
+		},50);
+	}/*,
 
 	temperature: function(temp) {
 
@@ -58,7 +151,7 @@ var Communication_Arduino = {
 		temp = seriesresistor / (1023 / temp - 1);
 
 		return (1.0 / ((Math.log(temp / thermistornominal) / bcoefficient) + (1.0/(temperaturenominal + 273.15)))) - 273.15 ;
-	}
+	}*/
 };
 
 module.exports = Communication_Arduino;
